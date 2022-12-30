cdfc258a9273416187de522979e238484a7f9a39
Rithin Ashikh <rithinashikhev@gmail.com>
2022-12-30 06:55:14 +0530

    first commit

---
+++ register/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from app1 import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.LoginPage, name='login'),
+    path('home/', views.HomePage, name='home'),
+    path('logout/', views.LogoutPage, name='logout'),
+]
